1b28310ad7dab40d0749a356a56659e1c093c36f
Бушуев Александр <sanekbusuev79@gmail.com>
2026-03-01 15:55:03 +0400

    feat: добавлена практика 28.02

---
+++ course-1/practices/28.02/28.02/Program.cs
@@ -0,0 +1,19 @@
+﻿{
+    int a = 12;
+    int b = 5;
+    Console.WriteLine($"a = {a}");
+    Console.WriteLine($"b = {b}");
+    int sum = a + b;
+    int diff = a - b;
+    int prod = a * b;
+    int div = a / b;
+    int mod = a % b;
+    Console.WriteLine($"Сумма: {a} + {b} = {sum}");
+    Console.WriteLine($"Разность: {a} - {b} = {diff}");
+    Console.WriteLine($"Произведение: {a} * {b} = {prod}");
+    Console.WriteLine($"Частное : {a} / {b} = {div}");
+    Console.WriteLine($"Остаток от деления: {a} % {b} = {mod}");
+    Console.Write("Введите ваше имя:");
+    string name = Console.ReadLine();
+    Console.WriteLine($"Привет,{name}!");
+}
