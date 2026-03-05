371350e734c3fba4f19ad9a33146651e0f56b0df
Бушуев Александр <sanekbusuev79@gmail.com>
2026-03-05 20:26:10 +0400

    feat: добавлены изменения в практику 28.02

--- course-1/practices/28.02/28.02/obj/Debug/net10.0/28.02.AssemblyInfo.cs
+++ course-1/practices/28.02/28.02/obj/Debug/net10.0/28.02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("28.02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8a44ddd63b8fc79726864b05300e3c0b270afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("28.02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("28.02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
